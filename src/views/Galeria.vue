0749acab4656f53c430e5b05d6fc5345684b7c45
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-02-04 18:52:41 +0100

    Nuevo carrusel para galeria

--- src/views/Galeria.vue
+++ src/views/Galeria.vue
@@ -2,46 +2,36 @@
     <div class="container">
         <h1 class="titulo">Galería</h1>
         <div class="row">
-            <div class="col-12 col-lg-6 col-md-6">
-                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="col-12 col-lg-12 col-md-12">
+                <div class="carrusel">
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
+      <img src="../assets/carousel/foto1.png" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
+      <img src="../assets/carousel/foto2.png" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
+      <img src="../assets/carousel/foto3.png" class="d-block w-100" alt="...">
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-            </div>
+</div> <!-- Fin carrusel -->
+            </div> <!-- FIN col-12 col-lg-6 col-md-6 -->
         </div><!-- FIN ROW -->
     </div>
 </template>
@@ -56,5 +46,9 @@ export default { name: 'Galeria' }
     color: white;
     border-radius: 10px;
     padding: 3px;
+}
+
+.carrusel,img {
+    height: 550px;
 }
 </style>
