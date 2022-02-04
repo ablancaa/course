1b5796f1678eb0fe0652f95606d041d2d738202d
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-02-04 19:04:18 +0100

    Implementación de galería

--- src/views/Galeria.vue
+++ src/views/Galeria.vue
@@ -9,16 +9,24 @@
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="../assets/carousel/foto1.png" class="d-block w-100" alt="...">
+      <img src="../assets/carousel/foto1.png" class="d-block w-100 h-50" alt="foto1">
     </div>
     <div class="carousel-item">
-      <img src="../assets/carousel/foto2.png" class="d-block w-100" alt="...">
+      <img src="../assets/carousel/foto2.png" class="d-block w-100 h-50" alt="foto2">
     </div>
     <div class="carousel-item">
-      <img src="../assets/carousel/foto3.png" class="d-block w-100" alt="...">
+      <img src="../assets/carousel/foto3.png" class="d-block w-100 h-50" alt="foto3">
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/carousel/foto4.png" class="d-block w-100 h-50" alt="foto4">
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/carousel/foto5.png" class="d-block w-100 h-50" alt="foto5">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -50,5 +58,11 @@ export default { name: 'Galeria' }
 
 .carrusel,img {
     height: 550px;
+}
+
+@media (min-width: 576px) {
+    .carrusel, img{
+        height: 30%;
+    }
 }
 </style>
