6baf34d669910f659044eee23378953106ccf346
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-02-04 18:23:18 +0100

    Nueva página Galeria

---
+++ src/views/Galeria.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>Galería</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Galeria'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
